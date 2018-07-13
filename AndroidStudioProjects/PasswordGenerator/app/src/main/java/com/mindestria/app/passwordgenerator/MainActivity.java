2a41d17d33d2284bf825804787882d6ba429b302
hreshtaksensei <patrickangel.ndri@gmail.com>
2018-07-13 07:46:03 +0000

    Init Commit

--- AndroidStudioProjects/PasswordGenerator/app/src/main/java/com/mindestria/app/passwordgenerator/MainActivity.java
+++ AndroidStudioProjects/PasswordGenerator/app/src/main/java/com/mindestria/app/passwordgenerator/MainActivity.java
@@ -6,8 +6,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.app.Dialog;
+import android.widget.Button; 
 
 public class MainActivity extends AppCompatActivity {
 
